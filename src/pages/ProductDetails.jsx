613fa2d3692d7e7ed55cf25616084d3b6de025a1
ang3lcr <ang3lcortes@outlook.com>
2022-10-07 02:13:59 -0500

    styled-productDetail

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import { Col, ListGroup, Row } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 import { Link, useParams } from 'react-router-dom'
+import '../styles/productDetails.css'
+
 
 function ProductDetails() {
 
@@ -18,17 +20,23 @@ function ProductDetails() {
   return (
     <Row>
       <Col>
+      <div className='main'>
+
         <h1>{productDetail?.title}</h1>
-        <img src={productDetail?.productImgs[1]}/>
+            <img  className="product-detail-img" src={productDetail?.productImgs[1]}/>
+          <h2>{productDetail?.price}</h2>
+        <p className='description'>{productDetail?.description}</p>
+      </div>
+        
       </Col>
       <Col lg={3}>
-          <ListGroup variant="flush">
+          <ListGroup variant="flush" className='related-products'>
           {
               relatedProducts.map(product => (
                 <ListGroup.Item key={product.id}>
                   <Link to={`/product/${product.id}`}>
+                    {product.title}<br/>
                     <img src={product.productImgs[2]}/>
-                    {product.title}
                     </Link>
                 </ListGroup.Item>
                 
